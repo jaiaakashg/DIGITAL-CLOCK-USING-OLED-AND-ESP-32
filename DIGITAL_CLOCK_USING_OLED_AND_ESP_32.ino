d742b955c7304f34f3f470f3367510f61825be1a
jaiaakashg <jaiaakashg28210@gmail.com>
2024-07-08 17:57:49 +0530

    Add files via upload

---
+++ DIGITAL_CLOCK_USING_OLED_AND_ESP_32.ino
@@ -0,0 +1,50 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <WiFi.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
+
+const char* ssid = "YOUR WIFI SSID";
+const char* password = "YOUR PASSWORD";
+
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "pool.ntp.org", 0, 60000);
+#define SCREEN_WIDTH 128 
+#define SCREEN_HEIGHT 64
+#define OLED_RESET    -1 
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+void setup() {
+  Serial.begin(115200);
+
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { 
+    Serial.println(("SSD1306 allocation failed"));
+    for(;;);
+  }
+
+ 
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to WiFi");
+
+  timeClient.begin();
+
+  display.clearDisplay();
+}
+
+void loop() {
+  timeClient.update();
+  String formattedTime = timeClient.getFormattedTime();
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 10);
+  display.println(formattedTime);
+  display.display();
+  delay(1000);
+}
+
